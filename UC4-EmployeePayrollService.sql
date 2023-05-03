bbae5e9900b3952ff648388e3cc46ead4aca809d
kiran68 <kiranurshil88@gmail.com>
2023-05-03 14:20:05 +0530

    Add: Query Perform To Retrive All Data

---
+++ UC4-EmployeePayrollService.sql
@@ -0,0 +1,32 @@
+create database payroll_service;
+show databases;
+use payroll_service;
+
+------------------- UC2 ------------------------
+Create table employee_payroll (
+id int not null auto_increment,
+ name varchar(60) not null, 
+ salary double);
+ 
+ describe employee_payroll;
+ select * from emoloyee_payroll;
+ 
+ ------------------------------ UC3 ------------------------------------
+ 
+ insert into employee_payroll(name, salary, start) values
+ ('kiran', 40000.00,'2020-04-03'),
+('Altaf', 50000.00, '2019-12-14'),
+('Ravi',50000.00,'2018-01-02'),
+('Aditi',50000.00,'2022-12-05');
+
+select * from employee_payroll;
+insert into employee_payroll(name,salary,start) values ('Soumya',60000.00,'2019-05-28');
+select * from employee_payroll;
+select * from employee_payroll where name = Soumya; update employee_payroll set salary = 55000.00 where id = 5;
+delete from employee_payroll where id = 5;
+
+-------------------- UC4 -------------------
+
+select * from Employee_payroll;
+
+
